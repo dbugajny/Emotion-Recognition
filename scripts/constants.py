472c7ec6a9024d7a6b600883d0c5ec7f5755e48f
dbugajny <dawid.bugajny@gmail.com>
2023-04-22 09:10:30 +0100

    add tqdm

--- scripts/constants.py
+++ scripts/constants.py
@@ -15,17 +15,17 @@ TRANSFORMERS = [
 ]
 
 DATA_RAW_PATHS = [
-    Path("../data/01_raw/annotations"),
-    Path("../data/01_raw/bitalino"),
-    Path("../data/01_raw/fixations"),
-    Path("../data/01_raw/pupil_positions"),
-    Path("../data/01_raw/rating"),
+    Path("data/01_raw/annotations"),
+    Path("data/01_raw/bitalino"),
+    Path("data/01_raw/fixations"),
+    Path("data/01_raw/pupil_positions"),
+    Path("data/01_raw/rating"),
 ]
 
 DATA_INTERMEDIATE_PATHS = [
-    Path("../data/02_intermediate/annotations"),
-    Path("../data/02_intermediate/bitalino"),
-    Path("../data/02_intermediate/fixations"),
-    Path("../data/02_intermediate/pupil_positions"),
-    Path("../data/02_intermediate/rating"),
+    Path("data/02_intermediate/annotations"),
+    Path("data/02_intermediate/bitalino"),
+    Path("data/02_intermediate/fixations"),
+    Path("data/02_intermediate/pupil_positions"),
+    Path("data/02_intermediate/rating"),
 ]
