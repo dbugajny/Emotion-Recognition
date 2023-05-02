04cd81f178461e1108790ed0022199f5865ebf09
dbugajny <dawid.bugajny@gmail.com>
2023-05-02 22:06:48 +0100

    tqdm

--- scripts/merge_pictures.py
+++ scripts/merge_pictures.py
@@ -17,7 +17,12 @@ def merge_ratings_with_key_pictures():
     rating_transformer = TRANSFORMERS["rating"]
 
     DATA_PRIMARY_PATHS["rating"].mkdir(parents=True, exist_ok=True)
-    for filepath in DATA_INTERMEDIATE_PATHS["rating"].iterdir():
+
+    for filepath in tqdm(
+        DATA_INTERMEDIATE_PATHS["rating"].iterdir(),
+        total=len(list(DATA_INTERMEDIATE_PATHS["rating"].iterdir().iterdir())),
+        desc=f"Processing {DATA_INTERMEDIATE_PATHS['rating'].stem} directory.",
+    ):
         df_rating = pd.read_parquet(filepath)
         df_rating = rating_transformer.merge_with_key_pictures(df_rating, key_pictures)
         df_rating.to_parquet(DATA_PRIMARY_PATHS["rating"] / Path(filepath.name))
@@ -49,5 +54,5 @@ def merge_fix_pp_with_annotations_and_ratings():
 
 
 if __name__ == "__main__":
-    # merge_ratings_with_key_pictures()
+    merge_ratings_with_key_pictures()
     merge_fix_pp_with_annotations_and_ratings()
