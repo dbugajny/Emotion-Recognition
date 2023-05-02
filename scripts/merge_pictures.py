73d33d264eef58c8afd16cab9d77b3c65d623454
dbugajny <dawid.bugajny@gmail.com>
2023-05-02 22:02:19 +0100

    Changes

--- scripts/merge_pictures.py
+++ scripts/merge_pictures.py
@@ -3,6 +3,7 @@ from constants import KEY_PICTURE_PATH, TRANSFORMERS, DATA_INTERMEDIATE_PATHS, D
 from pathlib import Path
 import numpy as np
 import re
+from tqdm import tqdm
 
 def prepare_key_pictures():
     key_pictures = pd.read_excel(KEY_PICTURE_PATH)
@@ -22,17 +23,23 @@ def merge_ratings_with_key_pictures():
 
 def merge_fix_pp_with_annotations_and_ratings():
     data_sources = ["fixations", "pupil_positions"]
-    rating_filepaths = DATA_INTERMEDIATE_PATHS["rating"].iterdir()
+    rating_filepaths = list(DATA_PRIMARY_PATHS["rating"].iterdir())
 
     for data_source in data_sources:
         DATA_PRIMARY_PATHS[data_source].mkdir(parents=True, exist_ok=True)
-        for filepath in DATA_INTERMEDIATE_PATHS[data_source].iterdir():
+
+        for filepath in tqdm(
+            DATA_INTERMEDIATE_PATHS[data_source].iterdir(),
+            total=len(list(DATA_INTERMEDIATE_PATHS[data_source].iterdir())),
+            desc=f"Processing {DATA_INTERMEDIATE_PATHS[data_source].stem} directory.",
+        ):
             df_annotations_filepath = DATA_INTERMEDIATE_PATHS["annotations"] / re.sub(r"_(f|p)\.", "_a.", filepath.name)
             df_annotations = pd.read_parquet(df_annotations_filepath)
 
             id_person = re.search(r"[Ss]\d{2,3}", filepath.stem).group(0).upper()
-            df_ratings_filepath = [r_fp for r_fp in rating_filepaths if r_fp.stem.upper().find(id_person)][0]
-            df_ratings = pd.read_parquet(df_ratings_filepath)
+
+            df_ratings_filepath = [r_fp for r_fp in rating_filepaths if r_fp.stem.upper().find(id_person)]
+            df_ratings = pd.read_parquet(df_ratings_filepath[0])
 
             df = pd.read_parquet(filepath)
             df = TRANSFORMERS[data_source].merge_with_annotations_and_ratings(df, df_annotations, df_ratings)
